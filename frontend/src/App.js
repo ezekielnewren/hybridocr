79ed5c824217d5b1a321d1e8cd9e3e8675e09617
Ezekiel Newren <ezekielnewren@gmail.com>
2024-03-24 20:20:07 -0600

    move cursor into the App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,12 +5,46 @@ import MatrixDisplay from './MatrixDisplay';
 
 function App() {
   const [size, setSize] = useState(10); // Default rows
+  const [cursor, setCursor] = useState({row: 0, col: 0});
   const [matrix, setMatrix] = useState(() => Core.newMatrix(size, size));
   const inputRef = useRef(null);
 
   const onDraw = () => {
       // your code here.
   }
+
+  const toggleCursor = useCallback(() => {
+      let old = matrix[cursor.row][cursor.col];
+      matrix[cursor.row][cursor.col] = old^1;
+  }, [cursor, setCursor]);
+
+  const moveCursor = useCallback((rowChange, colChange) => {
+      setCursor((prev) => ({
+          row: Core.clamp(prev.row+rowChange, 0, matrix.length-1),
+          col: Core.clamp(prev.col + colChange, 0, matrix[0].length-1)
+      }));
+  }, [matrix]);
+
+  useEffect(() => {
+      const handleKeyDown = (event) => {
+          switch (event.key) {
+            case 'ArrowUp': moveCursor(-1, 0); break;
+            case 'ArrowDown': moveCursor(1, 0); break;
+            case 'ArrowLeft': moveCursor(0, -1); break;
+            case 'ArrowRight': moveCursor(0, 1); break;
+            case ' ':
+                moveCursor(0, 0);
+                toggleCursor();
+                break;
+          }
+      };
+      document.addEventListener("keydown", handleKeyDown);
+
+      return () => {
+          document.removeEventListener("keydown", handleKeyDown);
+      };
+
+  }, [toggleCursor, moveCursor]);
 
   return (
     <div className="App">
@@ -35,7 +69,7 @@ function App() {
         />
       <button onClick={onDraw}>Generate</button>
       </div>
-      <MatrixDisplay matrix={matrix}/>
+      <MatrixDisplay matrix={matrix} cursor={cursor}/>
     </div>
   );
 }
--- frontend/src/MatrixDisplay.js
+++ frontend/src/MatrixDisplay.js
@@ -1,46 +1,13 @@
 import React, {useCallback, useEffect, useState} from 'react';
 import * as Core from './core';
 
-const MatrixDisplay = ({matrix}) => {
+const MatrixDisplay = ({matrix, cursor}) => {
 
-  const [cursor, setCursor] = useState({row: 0, col: 0});
+  // const [cursor, setCursor] = useState({row: 0, col: 0});
 
   const handleContextMenu = (e) => {
     e.preventDefault();
   };
-
-  const toggleCursor = useCallback(() => {
-      let old = matrix[cursor.row][cursor.col];
-      matrix[cursor.row][cursor.col] = old^1;
-  }, [cursor, setCursor]);
-
-  const moveCursor = useCallback((rowChange, colChange) => {
-      setCursor((prev) => ({
-          row: Core.clamp(prev.row+rowChange, 0, matrix.length-1),
-          col: Core.clamp(prev.col + colChange, 0, matrix[0].length-1)
-      }));
-  }, [matrix]);
-
-  useEffect(() => {
-      const handleKeyDown = (event) => {
-          switch (event.key) {
-            case 'ArrowUp': moveCursor(-1, 0); break;
-            case 'ArrowDown': moveCursor(1, 0); break;
-            case 'ArrowLeft': moveCursor(0, -1); break;
-            case 'ArrowRight': moveCursor(0, 1); break;
-            case ' ':
-                moveCursor(0, 0);
-                toggleCursor();
-                break;
-          }
-      };
-      document.addEventListener("keydown", handleKeyDown);
-
-      return () => {
-          document.removeEventListener("keydown", handleKeyDown);
-      };
-
-  }, [toggleCursor, moveCursor]);
 
   return (
     <div style={{ display: 'inline-block' }} onContextMenu={handleContextMenu}>
