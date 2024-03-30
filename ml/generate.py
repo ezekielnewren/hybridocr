d183fbfc51d794878ca510f120fe27c127902ca8
Ezekiel Newren <ezekielnewren@gmail.com>
2024-03-30 17:38:16 -0600

    benchmark how fast training data can be generated for many fonts

--- ml/generate.py
+++ ml/generate.py
@@ -1,4 +1,7 @@
+import string
+
 from PIL import Image, ImageDraw, ImageFont
+from tqdm import tqdm
 import time
 from core import *
 import sys
@@ -6,71 +9,96 @@ from pathlib import Path
 import json
 import cbor2
 import numpy as np
-
 from collections import OrderedDict
 
-# font_path = "/usr/share/fonts/liberation-serif/LiberationSerif-Regular.ttf"
 
-file_config = Path(sys.argv[1])
+class TextToImageGenerator:
+    def __init__(self, _wordlist, _font_path: list, _font_height):
+        self.wordlist = _wordlist
+        self.font_path = _font_path
+        self.font_height = _font_height
 
-with open(file_config) as fd:
-    config = json.loads(fd.read())
+        self.alphabet = set()
+        for word in self.wordlist:
+            for c in word:
+                self.alphabet.add(c)
+        t = [c for c in self.alphabet]
+        t.sort()
+        self.alphabet = "".join(t)
 
-with open(config["dictionary"]) as fd:
-    dictionary = [v.rstrip() for v in fd.readlines()]
+        self.font_map = OrderedDict()
 
-longest_word = max([len(v) for v in dictionary])
+        for font_path in self.font_path:
+            for font_size in range(1, 50 + 1):
+                try:
+                    font = ImageFont.truetype(font_path, font_size)
+                except OSError as e:
+                    if e.args[0] == "invalid pixel size":
+                        continue
+                    raise e
+                text_width, text_height = font.getsize(self.alphabet)
+                if text_height > self.font_height:
+                    font = ImageFont.truetype(font_path, font_size - 1)
 
-character_set = set()
-for word in dictionary:
-    for c in word:
-        character_set.add(c)
-t = [c for c in character_set]
-t.sort()
-character_set = "".join(t)
+                if text_height == self.font_height:
+                    self.font_map[font_path] = OrderedDict()
+                    self.font_map[font_path]["font"] = font
+                    self.font_map[font_path]["symbol"] = OrderedDict()
+                    for c in self.alphabet:
+                        w = draw_word(c, font, self.font_height)
+                        self.font_map[font_path]["symbol"][c] = np.array(w) / 255.0
+                    break
+
+    def generate_image_len(self):
+        return len(self.wordlist)*len(self.font_map)
+
+    def generate_image(self):
+        for word in self.wordlist:
+            for m in self.font_map.values():
+                yield np.concatenate([m["symbol"][c] for c in word], axis=1), word
+
+    def sample(self):
+        for k in self.font_map:
+            yield k, draw_word(k, self.font_map[k]["font"], self.font_height)
 
 
-font_height = 28
-pad = 5
+def main():
+    file_config = Path(sys.argv[1])
 
-train = OrderedDict()
+    with open(file_config) as fd:
+        config = json.loads(fd.read())
 
-method = 0
+    with open(config["dictionary"]) as fd:
+        dictionary = [v.rstrip() for v in fd.readlines()]
 
-for font_path in config["font"]:
-    font = None
-    for font_size in range(1, 50+1):
-        font = ImageFont.truetype(font_path, font_size)
-        text_width, text_height = font.getsize(character_set)
-        if text_height > font_height:
-            font = ImageFont.truetype(font_path, font_size-1)
-        elif text_height == font_height:
-            break
-
-    font_map = dict()
-    for c in character_set:
-        img = draw_word(c, font, font_height)
-        font_map[c] = np.array(img) / 255.0
-
-    # word = np.concatenate([font_map[c] for c in character_set], axis=1)
-    # img = array_to_image(word)
-    # show_image(img)
+    dictionary.append(string.ascii_letters+string.digits+string.punctuation)
 
     beg = time.time()
-    if method == 0:
-        for word in dictionary:
-            print(word)
-            t = [font_map[c] for c in word]
-            w = np.concatenate(t, axis=1)
-    elif method == 1:
-        for word in dictionary:
-            print(word)
-            image = draw_word(word, font)
-    elapsed = time.time()-beg
-    print("rate:", float(len(dictionary))/elapsed)
+    generator = TextToImageGenerator(dictionary, config["font"], 28)
+    elapsed = time.time() - beg
+    print("time to initialize:", elapsed)
 
-    dump = cbor2.dumps(train)
+    count = 0
+    beg = time.time()
+    for sample, label in tqdm(generator.generate_image(), total=generator.generate_image_len()):
+        count += 1
+        # print(label)
+        # img = array_to_image(array)
+        # show_image(img)
+        pass
 
-    break
+    # for path, img in generator.sample():
+    #     count += 1
+    #     print(path)
+    #     # show_image(img)
+    #     pass
+
+    elapsed = time.time() - beg
+    print("total time:", elapsed)
+    print("rate:", float(count) / elapsed)
+
+
+if __name__ == "__main__":
+    main()
 
 
