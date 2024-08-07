db2fd5fbb6141a29b099f2189ca3061b8c8ba8dd
Ezekiel Newren <ezekielnewren@gmail.com>
2024-08-07 13:35:52 -0600

    added a web example

---
+++ website/frontend/index.ts
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const greetingElement = document.getElementById('greeting');
+    if (greetingElement) {
+        greetingElement.textContent = 'Hybridocr, LLC';
+    }
+});
