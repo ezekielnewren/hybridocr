2c5bcca60133bf6f82744b833ae73e1ba9b4a345
Ezekiel Newren <ezekielnewren@gmail.com>
2024-09-20 18:30:54 -0600

    update the way the docker website image is created

--- build_website.sh
+++ build_website.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-docker build -f Dockerfile_website -t hybridocr_website .
+config=$(jq -cM . $HYBRIDOCR_CONFIG_FILE)
+
+if [ "$config" == "" ]; then
+  echo "HYBRIDOCR_CONFIG_FILE is empty or file does not exist"
+  exit 1
+fi
+
+docker_prefix=$(echo "$config" | jq -r .docker_prefix)
 VERSION=$(git describe --tags --abbrev=0)
-docker tag hybridocr_website:latest docker.ezekielnewren.com/hybridocr_website:$VERSION
+
+docker build -f Dockerfile_website -t hybridocr_website:$VERSION .
+docker tag hybridocr_website:$VERSION $docker_prefix/hybridocr_website:$VERSION
+echo
+echo docker push $docker_prefix/hybridocr_website:$VERSION
