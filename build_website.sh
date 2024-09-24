e7cf214cdd457f22b0109d54e505d0b2f6891853
Ezekiel Newren <ezekielnewren@gmail.com>
2024-09-24 17:09:33 -0600

    allow the docker prefix to be empty

--- build_website.sh
+++ build_website.sh
@@ -19,5 +19,5 @@ docker build -f Dockerfile_website -t hybridocr_website:$TAG . || exit 1
 docker tag hybridocr_website:$TAG $docker_prefix/hybridocr_website:$TAG || exit 1
 docker tag hybridocr_website:$TAG $docker_prefix/hybridocr_website:latest || exit 1
 echo
-echo "# gcloud container images delete $docker_prefix/hybridocr_website:$TAG"
+echo "# gcloud container images delete ${docker_prefix}hybridocr_website:$TAG"
 echo docker push $docker_prefix/hybridocr_website:$TAG
