368d558ca2526e610f97b6a779c3f3686795ae58
Ezekiel Newren <ezekielnewren@gmail.com>
2024-09-28 16:54:47 -0600

    build_website.sh provide an optional alternative image label

--- build_website.sh
+++ build_website.sh
@@ -11,13 +11,18 @@ if [ "$config" == "" ]; then
   exit 2
 fi
 
+LABEL="$1"
+if [ "$LABEL" == "" ]; then
+  LABEL="latest"
+fi
+
 docker_prefix=$(echo "$config" | jq -r .docker_prefix)
 TAG=$(git describe --tags --dirty --always)
 
 pushd website && npm run build && popd || exit 1
 docker build -f Dockerfile_website -t hybridocr_website:$TAG . || exit 1
 docker tag hybridocr_website:$TAG ${docker_prefix}hybridocr_website:$TAG || exit 1
-docker tag hybridocr_website:$TAG ${docker_prefix}hybridocr_website:latest || exit 1
+docker tag hybridocr_website:$TAG ${docker_prefix}hybridocr_website:$LABEL || exit 1
 echo
 echo "# gcloud container images delete ${docker_prefix}hybridocr_website:$TAG"
 echo docker push ${docker_prefix}hybridocr_website:$TAG
