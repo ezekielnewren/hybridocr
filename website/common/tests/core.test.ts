8fa458307c827a3243b6cebd8c0b1d2b5f4ebd2e
Ezekiel Newren <ezekielnewren@gmail.com>
2024-10-02 02:44:03 -0600

    use vitest, use @ldclabs/cose-ts without error

--- website/common/tests/core.test.ts
+++ website/common/tests/core.test.ts
@@ -1,27 +1,13 @@
-// import * as fs from 'fs';
-// import * as path from 'path';
-//
-// // import * from '../src/core';
-// import * as CoreModule from '../src/core';
-//
-// test('test greeter', () => {
-//   expect(CoreModule.greet("alice")).toBe("Hello, alice!");
-// });
-//
-//
-// test('extract images from pdf', async () => {
-//   const pdfFilePath = path.join(__dirname, 'test-assets', 'JWST generated.pdf');
-//   const buffer: Uint8Array = new Uint8Array(fs.readFileSync(pdfFilePath));
-//
-//   const arr = await CoreModule.extractImagesFromPDF(buffer);
-//
-//   expect(arr.length).toBe(3);
-//   expect(arr).toBeDefined()
-// })
-//
-// test('run google ocr', async () => {
-//
-// })
-//
-//
-//
+import { expect, test } from 'vitest'
+
+import * as core from "../src/core";
+
+test('core test', async () => {
+
+})
+
+test('random iv', async () => {
+    const iv = core.getRandomIV();
+    expect(iv).not.toBeNull();
+})
+
