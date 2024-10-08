9abc5615da5bde2eaf61bd6129e9b216a5a866b2
Ezekiel Newren <ezekielnewren@gmail.com>
2024-10-07 20:06:07 -0600

    always delete the config and reupload it

--- script/deploy_website.sh
+++ script/deploy_website.sh
@@ -21,7 +21,9 @@ docker_prefix=$(echo "$config" | jq -r .k8s.docker_prefix)
 
 kubectl create namespace $namespace 2>/dev/null
 kubectl -n $namespace get secret config-$namespace > /dev/null 2>&1; ec=$?
-if [ $ec -ne 0 ]; then
-  kubectl -n $namespace create secret generic config-$namespace --from-file=config.json=$CONFIG_FILE
+if [ $ec -eq 0 ]; then
+  kubectl -n $namespace delete secret config-$namespace
+  sleep 1
 fi
+kubectl -n $namespace create secret generic config-$namespace --from-file=config.json=$CONFIG_FILE
 helm install hw-$namespace ../helm --namespace $namespace --set domain=$domain --set docker_prefix=${docker_prefix}
