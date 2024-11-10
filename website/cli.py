ba0b332eb3146ac24b8f5e5f3a3667ec43a2cddd
Ezekiel Newren <ezekielnewren@gmail.com>
2024-11-10 16:11:02 -0700

    add argparse to cli

--- website/cli.py
+++ website/cli.py
@@ -1,4 +1,5 @@
 import asyncio
+import argparse
 
 from website import common
 import sys
@@ -7,9 +8,16 @@ from website.gmail import GmailClient
 
 
 async def main(argv):
+    parser = argparse.ArgumentParser(description="Run updates for: Gmail")
+    parser.add_argument('--update', action='store_true', help="Runs updates for: Gmail")
+    args = parser.parse_args(argv[1:])
     config = await common.get_config()
-    gmail = GmailClient(config)
-    await gmail.update_token(interactive=True)
+
+    if args.update:
+        gmail = GmailClient(config)
+        await gmail.update_token(interactive=True)
+    else:
+        parser.print_help()
 
 if __name__ == "__main__":
     asyncio.run(main(sys.argv))
