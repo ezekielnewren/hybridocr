21d4b27f87a295b77377a586f93d0644290e8ffb
Ezekiel Newren <ezekielnewren@gmail.com>
2024-11-18 16:30:54 -0700

    add the util.js to the header

---
+++ website/static/js/util.js
@@ -0,0 +1,10 @@
+
+function escapeHtml(unsafe) {
+    // https://stackoverflow.com/a/6234804
+    return unsafe
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+}
