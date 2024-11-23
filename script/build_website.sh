4a909c93415590f3a0e4633844a3461147f1157f
Ezekiel Newren <ezekielnewren@gmail.com>
2024-11-23 01:24:39 -0700

    fix how the scripts get the configuration for the given environment

--- script/build_website.sh
+++ script/build_website.sh
@@ -9,6 +9,10 @@ if [ "$CONFIG_FILE" == "" ]; then
   exit 1
 fi
 config=$(jq -cM . $CONFIG_FILE)
+export VAULT_ADDR=$(echo $config | jq -r .vault.VAULT_ADDR)
+export VAULT_TOKEN=$(echo $config | jq -r .vault.VAULT_TOKEN)
+export VAULT_CACERT=""
+config=$(vault kv get kv/env/$(vault token lookup | jq -r .data.meta.env) | jq .data.data)
 
 if [ "$config" == "" ]; then
   echo "$CONFIG_FILE is empty"
@@ -18,6 +22,13 @@ fi
 namespace=$(echo "$config" | jq -r .k8s.namespace)
 domain=$(echo "$config" | jq -r .webserver.domain[0])
 docker_prefix=$(echo "$config" | jq -r .k8s.docker_prefix)
+
+for v in $namespace $domain $docker_prefix; do
+  if [ "$v" == "" ] || [ "$v" == "null" ]; then
+    echo "config does not define all parameters"
+    exit 1
+  fi
+done
 
 LABEL="$namespace"
 if [ "$LABEL" == "" ]; then
