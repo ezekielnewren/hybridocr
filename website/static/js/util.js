7789ef2fe5d458bbb711493824ebc1a7d80439b6
Ezekiel Newren <ezekielnewren@gmail.com>
2024-11-24 18:12:10 -0700

    rework how showcase works for tryitout

--- website/static/js/util.js
+++ website/static/js/util.js
@@ -48,3 +48,58 @@ async function register(_id, email) {
         throw new Error(`Failed to submit email: ${response.status} ${response.statusText}`);
     }
 }
+
+class Showcase {
+    constructor(element) {
+        this.element = element;
+        this.image = null;
+        this.ocr = null;
+
+        // setup ui behavior
+        const tabButtonList = this.element.querySelectorAll(".tab-button");
+        for (const tabButton of tabButtonList) {
+            const eTextArea = this.element.querySelector("textarea");
+
+            tabButton.addEventListener("click", () => {
+                tabButtonList.forEach(btn => btn.classList.remove("active"));
+                tabButton.classList.add("active");
+
+                const type = tabButton.getAttribute("data-tab");
+                if (this.ocr == null) {
+                    eTextArea.value = "No data yet";
+                } else if (type === "text") {
+                    eTextArea.value = this.ocr.fullTextAnnotation.text;
+                } else if (type === "json") {
+                    eTextArea.value = JSON.stringify(this.ocr, null, 2);
+                } else {
+                    throw new Error("invalid tab button type");
+                }
+            })
+        }
+        tabButtonList.item(0).click();
+    }
+
+    getActiveTab() {
+        for (const tabButton of this.element.querySelectorAll(".tab-button")) {
+            if (tabButton.classList.contains("active")) {
+                return tabButton;
+            }
+        }
+    }
+
+    setImage(_image) {
+        this.image = _image;
+        const eImage = this.element.querySelector('img');
+        eImage.src = URL.createObjectURL(this.image);
+    }
+
+    getImage() {
+        return this.image;
+    }
+
+    setOcr(_ocr) {
+        this.ocr = _ocr;
+        this.getActiveTab().click();
+    }
+}
+
