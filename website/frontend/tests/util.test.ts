f011c40fd9cb857521e205a4cc1480b040a3ca24
Ezekiel Newren <ezekielnewren@gmail.com>
2024-12-05 14:47:26 -0700

    add 2 commented out tests that are really difficult

--- website/frontend/tests/util.test.ts
+++ website/frontend/tests/util.test.ts
@@ -5,6 +5,8 @@ describe("util", () => {
 
     test("rustargon2", async () => {
         const test_vector: [string, string, number, number, number, number, string][] = [
+            // ["abc123", "saltsaltsaltsalt", 8, 100000, 1, 8, "805ef5742dad2e5d"],
+            // ["abc123", "saltsaltsaltsalt", 65536, 100, 1, 8, "4ac02cad101aef49"],
             ["password", "saltsaltsaltsalt", 8192, 10, 1, 8, "60d5d3728e906b8e"],
             ["abc", "saltsaltsaltsalt", 8, 1, 1, 16, "3c71ee188cea4be3ba175abaaa42325f"],
             ["abc", "saltsaltsaltsalt", 16, 1, 2, 16, "459b0fe92c28b3144d7c35565b0a0273"],
