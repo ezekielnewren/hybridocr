34082ecd2b770e4ba1bd814b8919e7ff13e53ec6
Ezekiel Newren <ezekielnewren@gmail.com>
2024-12-07 16:15:36 -0700

    make perspective_transform compilable for webassembly

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,8 +1,10 @@
 use argon2::{Algorithm, Argon2, Params, Version};
 use wasm_bindgen::prelude::*;
 
-use image::{EncodableLayout, GrayImage, Luma};
+use image::{DynamicImage, EncodableLayout, GrayImage, ImageBuffer, Luma};
+use image::DynamicImage::ImageLuma8;
 use imageproc::geometric_transformations::{warp, Interpolation, Projection};
+use serde::{Serialize, Deserialize};
 
 #[wasm_bindgen]
 pub fn _argon2id(password: &[u8], salt: &[u8], m: u32, t: u32, p: u32, length: u32) -> Vec<u8> {
@@ -30,162 +32,201 @@ pub fn argon2(alg: Algorithm, password: &[u8], salt: &[u8], m: u32, t: u32, p: u
 }
 
 
-#[wasm_bindgen]
-struct PixelBuffer {
+#[derive(Serialize, Deserialize)]
+pub struct Point {
+    pub x: f32,
+    pub y: f32,
+}
+
+impl Clone for Point {
+    fn clone(&self) -> Self {
+        Self{ x: self.x, y: self.y }
+    }
+}
+
+impl Copy for Point {}
+
+
+pub fn midpoint(a: Point, b: Point) -> Point {
+    Point{x: (a.x+b.x)/2.0, y: (a.y+b.y)/2.0}
+}
+
+pub fn distance(a: Point, b: Point) -> f32 {
+    ((a.x-b.x).powi(2) + (a.y-b.y).powi(2)).sqrt()
+}
+
+
+pub fn deinterleave(src: &Vec<u8>, channels: u8) -> Result<Vec<u8>, String> {
+    let channel_size = src.len() / channels as usize;
+    let r = src.len() % channels as usize;
+    if r != 0 {
+        return Err(String::from("image size is not an integer multiple of channels"));
+    }
+    let mut dst = vec![0u8; src.len()];
+    for channel in 0..channels as usize {
+        for pixel in 0..channel_size {
+            dst[channel*channel_size+pixel] = src[pixel*channels as usize+channel];
+        }
+    }
+    Ok(dst)
+}
+
+pub fn interleave(src: &[u8], channels: u8) -> Result<Vec<u8>, String> {
+    let channel_size = src.len() / channels as usize;
+    let r = src.len() % channels as usize;
+    if r != 0 {
+        return Err(String::from("image size is not an integer multiple of channels"));
+    }
+    let mut dst = vec![0u8; src.len()];
+    for channel in 0..channels as usize {
+        for pixel in 0..channel_size {
+            dst[pixel*channels as usize+channel] = src[channel*channel_size+pixel];
+        }
+    }
+    Ok(dst)
+}
+
+
+#[derive(Serialize, Deserialize)]
+pub struct PixelBuffer {
     pub width: u32,
     pub height: u32,
     pub channels: u8,
     pub interleaved: bool,
+    pub data: Vec<u8>,
 }
 
-impl Clone for PixelBuffer {
-    fn clone(&self) -> Self {
-        Self {
-            width: self.width,
-            height: self.height,
-            channels: self.channels,
-            interleaved: self.interleaved,
-        }
-    }
-}
-
-impl Copy for PixelBuffer {
-
-}
 
 impl PixelBuffer {
-
-    pub fn new(width: u32, height: u32, channels: u8, interleaved: bool) -> Self {
-        Self {width, height, channels, interleaved}
-    }
-
-    pub fn as_channels(&self, src: Vec<u8>) -> Result<Vec<GrayImage>, String> {
+    pub fn as_channels(&self) -> Result<Vec<GrayImage>, String> {
         let mut dst: Vec<GrayImage> = Vec::new();
-        let size: usize = (self.width*self.height) as usize;
-        let channel_size = size/self.channels as usize;
-        let r = size%self.channels as usize;
-        if r != 0 {
-            return Err(String::from("image size is not an integer multiple of channels"));
-        }
+        let channel_size = self.data.len()/self.channels as usize;
+        let t: Vec<u8>;
+        let planar: &[u8];
         if self.interleaved {
-            for channel in 0usize..self.channels as usize {
-                let mut t = Vec::<u8>::new();
-                for pixel in 0..channel_size {
-                    t.push(src[pixel*self.channels as usize+channel]);
-                }
-                dst.push(GrayImage::from_vec(self.width, self.height, t).unwrap());
-            }
+            t = deinterleave(&self.data, self.channels)?;
+            planar = t.as_slice();
         } else {
-            dst = Vec::new();
-            for channel in 0usize..self.channels as usize {
-                let off = channel*channel_size;
-                let v = Vec::from(&src[off..off+channel_size]);
-                let img = GrayImage::from_vec(self.width, self.height, v).unwrap();
-                dst.push(img);
-            }
+            planar = self.data.as_slice();
+        }
+        for channel in 0usize..self.channels as usize {
+            let off = channel*channel_size;
+            let pixel_data: Vec<u8> = Vec::from(&planar[off..off+channel_size]);
+            assert_eq!(self.width*self.height, pixel_data.len() as u32);
+            let img: GrayImage = GrayImage::from_vec(self.width, self.height, pixel_data).unwrap();
+            dst.push(img);
         }
         Ok(dst)
     }
 
-    pub fn from_channels(image: Vec<GrayImage>, interleave: bool) -> Result<(Self, Vec<u8>), String> {
-        let channels = image.len();
-        let width = image[0].width() as usize;
-        let height = image[0].height() as usize;
-        for i in 1..image.len() {
-            if image[i].width() as usize != width {
+    pub fn as_dynamic_image(&self) -> Result<DynamicImage, String> {
+        let t: Vec<u8>;
+        let planar: &[u8];
+        if self.interleaved {
+            t = deinterleave(&self.data, self.channels)?;
+            planar = t.as_slice();
+        } else {
+            planar = self.data.as_slice();
+        }
+
+        if self.channels == 1 {
+            Ok(DynamicImage::ImageLuma8(ImageBuffer::from_vec(self.width, self.height, planar.to_vec()).unwrap()))
+        } else if self.channels == 3 {
+            Ok(DynamicImage::ImageRgb8(ImageBuffer::from_vec(self.width, self.height, planar.to_vec()).unwrap()))
+        } else if self.channels == 4 {
+            Ok(DynamicImage::ImageRgba8(ImageBuffer::from_vec(self.width, self.height, planar.to_owned()).unwrap()))
+        } else {
+            Err(String::from("Illegal parameters"))
+        }
+    }
+
+    pub fn from_channels(channel: &Vec<GrayImage>, interleaved: bool) -> Result<Self, String> {
+        let width = channel[0].width() as usize;
+        let height = channel[0].height() as usize;
+        for i in 1..channel.len() {
+            if channel[i].width() as usize != width {
                 return Err(String::from("image width of all channels must be the same"));
             }
-            if image[i].height() as usize != height {
+            if channel[i].height() as usize != height {
                 return Err(String::from("image height of all channels must be the same"));
             }
         }
 
         let mut dst: Vec<u8>;
-        if interleave {
-            let size: usize = width*height;
-            dst = vec![0u8; size];
-            let channel_size = size/channels;
-            let r = size%channels;
-            if r != 0 {
-                return Err(String::from("image size is not an integer multiple of channels"));
+        if interleaved {
+            let mut src = Vec::<u8>::new();
+            for c in channel {
+                src.extend_from_slice(c.as_bytes());
             }
-            for channel in 0usize..channels {
-                let t = image[channel].as_bytes();
-                for pixel in 0..channel_size {
-                    dst[pixel*channels+channel] = t[pixel];
-                }
-            }
+            dst = interleave(src.as_slice(), channel.len() as u8)?;
         } else {
             dst = Vec::new();
-            for channel in 0usize..channels {
-                dst.copy_from_slice(image[channel].as_bytes());
+            for c in channel {
+                dst.extend_from_slice(c.as_bytes());
             }
         }
-        Ok((Self {
+        Ok(Self {
             width: width as u32,
             height: height as u32,
-            channels: channels as u8,
-            interleaved: interleave,
-        }, dst))
+            channels: channel.len() as u8,
+            interleaved,
+            data: dst
+        })
     }
 }
 
-pub fn midpoint(a: (f32, f32), b: (f32, f32)) -> (f32, f32) {
-    ((a.0+b.0)/2.0, (a.1+b.1)/2.0)
-}
 
-pub fn distance(a: (f32, f32), b: (f32, f32)) -> f32 {
-    ((a.0-b.0).powi(2) + (a.1-b.1).powi(2)).sqrt()
-}
-
-
-// #[wasm_bindgen]
-pub fn pertrans(image: PixelBuffer, data: Vec<u8>, src_points: [(f32, f32); 4]) -> Result<(PixelBuffer, Vec<u8>), String> {
-    let warp = perspective_transform(image, data, src_points)?;
-    Ok(warp)
+#[derive(Serialize, Deserialize)]
+pub struct Quadrilateral {
+    pub tl: Point,
+    pub tr: Point,
+    pub br: Point,
+    pub bl: Point,
 }
 
 
-pub fn perspective_transform(
-    image: PixelBuffer,
-    src: Vec<u8>,
-    src_points: [(f32, f32); 4],
-) -> Result<(PixelBuffer, Vec<u8>), String> {
-    let tl = src_points[0];
-    let tr = src_points[1];
-    let br = src_points[2];
-    let bl = src_points[3];
+impl Quadrilateral {
+    pub fn as_array(&self) -> [(f32, f32); 4] {
+        [(self.tl.x, self.tl.y), (self.tr.x, self.tr.y), (self.br.x, self.br.y), (self.bl.x, self.bl.y)]
+    }
 
-    let left_mid = midpoint(tl, bl);
-    let right_mid = midpoint(tr, br);
-    let width = distance(left_mid, right_mid);
+    pub fn dst_rect(&self) -> [(f32, f32); 4] {
+        let left_mid = midpoint(self.tl, self.bl);
+        let right_mid = midpoint(self.tr, self.br);
+        let width = distance(left_mid, right_mid);
 
-    let top_mid = midpoint(tl, tr);
-    let bot_mid = midpoint(bl, br);
-    let height = distance(top_mid, bot_mid);
+        let top_mid = midpoint(self.tl, self.tr);
+        let bot_mid = midpoint(self.bl, self.br);
+        let height = distance(top_mid, bot_mid);
+
+        [(0.0, 0.0), (width, 0.0), (width, height), (0.0, height)]
+    }
+}
 
 
-    // Define destination points as a rectangle
-    let dst_points = [
-        (0.0, 0.0),
-        (width, 0.0),
-        (width, height),
-        (0.0, height),
-    ];
+#[wasm_bindgen]
+pub fn perspective_transform(_image: JsValue, _quad: JsValue) -> JsValue {
+    let image = serde_wasm_bindgen::from_value::<PixelBuffer>(_image).unwrap();
+    let quad = serde_wasm_bindgen::from_value::<Quadrilateral>(_quad).unwrap();
+    let result = _perspective_transform(image, quad);
+    match result {
+        Ok(v) => serde_wasm_bindgen::to_value(&v).unwrap(),
+        Err(e) => serde_wasm_bindgen::to_value(&e).unwrap(),
+    }
+}
 
-    // Compute the perspective projection
-    let interleaved = image.interleaved;
-    let projection = Projection::from_control_points(src_points, dst_points).unwrap();
-    let default = Luma::from([0u8]);
+
+pub fn _perspective_transform(image: PixelBuffer, quad: Quadrilateral) -> Result<PixelBuffer, String> {
+    let projection = Projection::from_control_points(quad.as_array(), quad.dst_rect()).unwrap();
     let mut output = Vec::<GrayImage>::new();
-    for img in image.as_channels(src)? {
+    for img in image.as_channels()? {
         let warped_image = warp(
             &img,
             &projection,
             Interpolation::Bicubic,
-            default,
+            Luma::from([0u8]),
         );
         output.push(warped_image);
     }
-    PixelBuffer::from_channels(output, interleaved)
+    PixelBuffer::from_channels(&output, image.interleaved)
 }
--- wasm/tests/wasm_tests.rs
+++ wasm/tests/wasm_tests.rs
@@ -1,9 +1,15 @@
-use hybridocr::_argon2id;
+use hybridocr::{_argon2id, _perspective_transform};
 
 #[cfg(test)]
 mod tests {
+    use std::fs::File;
+    use std::io::{Cursor, Write};
+    use std::path::Path;
     use std::time::Instant;
+    use image::{ExtendedColorType, ImageReader, ImageFormat};
+    use hybridocr::{PixelBuffer, Point, Quadrilateral};
     use super::*;
+
 
     #[test]
     fn test_argon2() {
@@ -21,4 +27,38 @@ mod tests {
         println!("{}", result);
         println!("{}", duration.as_secs_f64());
     }
+
+    #[test]
+    fn test_perspective_transform() {
+        let fd = ImageReader::open("../tests/file/ocr_sample_from_smartphone.webp").unwrap();
+        let img = fd.decode().unwrap();
+
+        let pb = PixelBuffer {
+            width: img.width(),
+            height: img.height(),
+            channels: img.color().channel_count(),
+            interleaved: true,
+            data: Vec::from(img.as_bytes()),
+        };
+        assert_eq!((pb.width * pb.height * pb.channels as u32) as usize, pb.data.len());
+
+        let quad = Quadrilateral {
+            tl: Point{x: 50.0,   y: 335.0},
+            tr: Point{x: 1076.0, y: 305.0},
+            br: Point{x: 1130.0, y: 1688.0},
+            bl: Point{x: 29.0,   y: 1690.0},
+        };
+
+
+        let result = _perspective_transform(pb, quad).unwrap();
+        let out = result.as_dynamic_image().unwrap();
+
+        let mut bytes = Vec::<u8>::new();
+        let mut buffer = Cursor::new(&mut bytes);
+        let _ = out.write_to(&mut buffer, ImageFormat::WebP);
+
+        let mut fdo = File::create(Path::new("/tmp/ocr_sample_from_smartphone_corrected.webp")).unwrap();
+        fdo.write(bytes.as_slice()).unwrap();
+    }
+
 }
