fc414f6e762549250e7a3d01d4f1ee1f1b5d0f06
Ezekiel Newren <ezekielnewren@gmail.com>
2024-12-10 14:24:24 -0700

    implement bicubic interpolation

--- wasm/tests/wasm_tests.rs
+++ wasm/tests/wasm_tests.rs
@@ -104,7 +104,7 @@ mod tests {
             bl: Point{x: 29.0,   y: 1690.0},
         };
 
-        let result = _pt(pb, quad, Interp::Biquadratic as isize).unwrap();
+        let result = _pt(pb, quad, Interp::Bicubic as isize).unwrap();
         let out = result.as_dynamic_image().unwrap();
 
         assert_eq!(src_interleaved, result.interleaved);
