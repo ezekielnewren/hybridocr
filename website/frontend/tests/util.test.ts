713a59e5d36d747eb7aced16c1b0f26366771396
Ezekiel Newren <ezekielnewren@gmail.com>
2025-01-01 00:20:39 -0700

    add hkdf tests for SHA-384 and SHA-512

--- website/frontend/tests/util.test.ts
+++ website/frontend/tests/util.test.ts
@@ -77,10 +77,25 @@ describe("util", () => {
     })
     
     test("hkdf", async () => {
-        // https://datatracker.ietf.org/doc/html/rfc5869#appendix-A
-
         // okm, hash, ikm, salt, info, length
         let tv: [string, string, string, string, string, number][] = [
+            [
+                "e00f9e18a24f2ae769846de7e5c65380981a0fd324c7e371340cb9a5454b01e4",
+                "SHA-512",
+                "048458114dafd3d7b3de4aef7a2cf7ba",
+                util.toHex("salt", true) as string,
+                util.toHex("info", true) as string,
+                32
+            ],
+            [
+                "b4c206dd9156c18d29598ffccbf0a64c0325c0b131144d7d1a09fa84ccbfdd20",
+                "SHA-384",
+                "bd8857d2500f6a136b4d02aad3a5dfca",
+                util.toHex("salt", true) as string,
+                util.toHex("info", true) as string,
+                32
+            ],
+            // https://datatracker.ietf.org/doc/html/rfc5869#appendix-A
             [
                 "3cb25f25faacd57a90434f64d0362f2a2d2d0a90cf1a5a4c5db02d56ecc4c5bf34007208d5b887185865",
                 "SHA-256",
@@ -97,6 +112,46 @@ describe("util", () => {
                 "b0b1b2b3b4b5b6b7b8b9babbbcbdbebfc0c1c2c3c4c5c6c7c8c9cacbcccdcecfd0d1d2d3d4d5d6d7d8d9dadbdcdddedfe0e1e2e3e4e5e6e7e8e9eaebecedeeeff0f1f2f3f4f5f6f7f8f9fafbfcfdfeff",
                 82
             ],
+            [
+                "8da4e775a563c18f715f802a063c5a31b8a11f5c5ee1879ec3454e5f3c738d2d9d201395faa4b61a96c8",
+                "SHA-256",
+                "0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b",
+                "",
+                "",
+                42
+            ],
+            [
+                "085a01ea1b10f36933068b56efa5ad81a4f14b822f5b091568a9cdd4f155fda2c22e422478d305f3f896",
+                "SHA-1",
+                "0b0b0b0b0b0b0b0b0b0b0b",
+                "000102030405060708090a0b0c",
+                "f0f1f2f3f4f5f6f7f8f9",
+                42
+            ],
+            [
+                "0bd770a74d1160f7c9f12cd5912a06ebff6adcae899d92191fe4305673ba2ffe8fa3f1a4e5ad79f3f334b3b202b2173c486ea37ce3d397ed034c7f9dfeb15c5e927336d0441f4c4300e2cff0d0900b52d3b4",
+                "SHA-1",
+                "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f404142434445464748494a4b4c4d4e4f",
+                "606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e7f808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9fa0a1a2a3a4a5a6a7a8a9aaabacadaeaf",
+                "b0b1b2b3b4b5b6b7b8b9babbbcbdbebfc0c1c2c3c4c5c6c7c8c9cacbcccdcecfd0d1d2d3d4d5d6d7d8d9dadbdcdddedfe0e1e2e3e4e5e6e7e8e9eaebecedeeeff0f1f2f3f4f5f6f7f8f9fafbfcfdfeff",
+                82
+            ],
+            [
+                "0ac1af7002b3d761d1e55298da9d0506b9ae52057220a306e07b6b87e8df21d0ea00033de03984d34918",
+                "SHA-1",
+                "0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b",
+                "",
+                "",
+                42
+            ],
+            [
+                "2c91117204d745f3500d636a62f64f0ab3bae548aa53d423b0d1f27ebba6f5e5673a081d70cce7acfc48",
+                "SHA-1",
+                "0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c",
+                "",
+                "",
+                42
+            ]
         ];
 
 
